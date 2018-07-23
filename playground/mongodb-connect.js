8efc06996cf655175a691b8f3c8d838cf04f63e0
Zach Samuelson <zsam56@gmail.com>
2018-07-23 10:25:09 -0400

    update

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,6 +1,7 @@
 // const MongoClient = require('mongodb').MongoClient;
 const {MongoClient, ObjectID} = require('mongodb');
 
+
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, client) => {
   if(error) {
     return console.log("Unable to connect to MongoDB server.");
