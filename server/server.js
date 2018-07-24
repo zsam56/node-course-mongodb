95e8b8b17ea52b271760d53ffc479a37ff29a036
Zach Samuelson <zsam56@gmail.com>
2018-07-24 19:57:23 -0400

    test post todos

--- server/server.js
+++ server/server.js
@@ -17,10 +17,12 @@ app.post('/todos', (req, res) => {
   todo.save().then((doc) => {
     res.send(doc);
   }, (err) => {
-    res.status(400).send(e);
+    res.status(400).send(err);
   });
 });
 
 app.listen(3000, () => {
   console.log('Started on port 3000')
 });
+
+module.exports = {app};
